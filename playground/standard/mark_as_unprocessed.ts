a4e65672f54354631e22631bc9da1376ad7e86a6
RubensJr21 <junior.correa2001@gmail.com>
2025-09-22 20:19:45 -0300

    add standard unmark_as_processed feature

---
+++ playground/standard/mark_as_unprocessed.ts
@@ -0,0 +1,53 @@
+import * as bup from "@data_functions/balance_update_pipeline";
+import * as iv from "@data_functions/item_value";
+import * as std from "@data_functions/standard";
+import { db } from "@database/db-instance";
+import { standard } from "@database/schema";
+
+export async function mark_as_processed(
+	standard_id: typeof standard.$inferSelect.id
+) {
+	const standard_founded = await std.get(db, standard_id);
+	if (standard_founded === undefined) {
+		throw new Error(
+			`Nenhuma transação padrão encontrada (${standard_founded})`
+		);
+	}
+
+	await iv.mark_as_unprocessed(db, standard_founded.item_value_id);
+
+	// ======================================
+	// POST MARKED
+	// ======================================
+	// atualizar saldo
+
+	const month = standard_founded.scheduled_at.getMonth();
+	const year = standard_founded.scheduled_at.getFullYear();
+
+	if (standard_founded.transfer_method_code === "cash") {
+		// Fluxo do dinheiro
+		bup.balance_cash_update_pipeline(
+			db,
+			{
+				month,
+				year,
+				amount: standard_founded.amount,
+				cashflow_type: standard_founded.cashflow_type,
+			},
+			false
+		);
+	} else {
+		// Fluxo do banco
+		bup.balance_bank_update_pipeline(
+			db,
+			{
+				month,
+				year,
+				amount: standard_founded.amount,
+				cashflow_type: standard_founded.cashflow_type,
+				transaction_instrument_id: standard_founded.transaction_instrument_id,
+			},
+			false
+		);
+	}
+}
