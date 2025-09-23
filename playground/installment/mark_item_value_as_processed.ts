529b33e0122e28750e28bf4bae81d02dc49531a3
RubensJr21 <junior.correa2001@gmail.com>
2025-09-23 09:55:58 -0300

    add mark-item-value-as-processed to installment

---
+++ playground/installment/mark_item_value_as_processed.ts
@@ -0,0 +1,53 @@
+import * as bup from "@data_functions/balance_update_pipeline";
+import * as imt from "@data_functions/installment";
+import * as iv from "@data_functions/item_value";
+import { db } from "@database/db-instance";
+import { installment, itemValue } from "@database/schema";
+
+export async function mark_item_value_as_processed(
+	installment_id: typeof installment.$inferSelect.id,
+	item_value_id: typeof itemValue.$inferSelect.id
+) {
+	const installment_founded = await imt.get(db, installment_id);
+	if (installment_founded === undefined) {
+		throw new Error(
+			`Nenhuma transação parcelada encontrada (${installment_founded})`
+		);
+	}
+
+  const item_value = await imt.get_item_value(db, installment_id, item_value_id)
+  if (item_value === undefined) {
+    throw new Error(
+      `Nenhum valor de parcela encontrado (${item_value})`
+    );
+  }
+
+	await iv.mark_as_processed(db, item_value.id);
+
+	// ======================================
+	// POST MARKED
+	// ======================================
+	// atualizar saldo
+
+	const month = item_value.scheduled_at.getMonth();
+	const year = item_value.scheduled_at.getFullYear();
+
+	if (installment_founded.transfer_method_code === "cash") {
+		// Fluxo do dinheiro
+		bup.balance_cash_update_pipeline(db, {
+			month,
+			year,
+			amount: item_value.amount,
+			cashflow_type: installment_founded.cashflow_type,
+		});
+	} else {
+		// Fluxo do banco
+		bup.balance_bank_update_pipeline(db, {
+			month,
+			year,
+			amount: item_value.amount,
+			cashflow_type: installment_founded.cashflow_type,
+			transaction_instrument_id: installment_founded.transaction_instrument_id,
+		});
+	}
+}
